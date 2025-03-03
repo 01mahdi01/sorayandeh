1ecd66a9019cf73fc4da1133ca967151d04bf387
mahdi <m.mahdi.sa.81@gmail.com>
2025-03-02 21:51:47 -0800

    add get financial logs endpoint

--- sorayandeh/finance/urls.py
+++ sorayandeh/finance/urls.py
@@ -1,11 +1,12 @@
 from azbankgateways.views.banks import go_to_bank_gateway
 from django.urls import path
-from .apis import RequestPaymentUrl,CallbackPaymentUrl
+from .apis import RequestPaymentUrl,CallbackPaymentUrl,GetFinancialLogs
 
 urlpatterns = [
 
     path('get_bank_url/', RequestPaymentUrl.as_view(),name="get_bank_url"),
     path('callback_gateway/', CallbackPaymentUrl.as_view(),name="callback_gateway"),
+    path('get_financial_logs/', GetFinancialLogs.as_view(),name="get_financial_logs"),
 
 
 ]
