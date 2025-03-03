b23a9e8c7b66609488e8a127751a177d80d92c06
mahdi <m.mahdi.sa.81@gmail.com>
2025-03-02 17:32:14 -0800

    admin for finance

--- sorayandeh/finance/admin.py
+++ sorayandeh/finance/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import FinancialLogs
 
-# Register your models here.
+@admin.register(FinancialLogs)
+class FinancialLogsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'campaign', 'transaction')
+    search_fields = ('user__username', 'campaign__name', 'transaction__tracking_code')
+    list_filter = ('campaign',)
+
+    def get_queryset(self, request):
+        """Optimize queryset to avoid unnecessary queries."""
+        return super().get_queryset(request).select_related('user', 'campaign', 'transaction')
+
